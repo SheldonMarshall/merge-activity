36983d409270e2c9d0f4e39f3a477b861db5b28e
SheldonMarshall <smarsha1@unca.edu>
2024-02-06 10:30:57 -0500

    added Feature A to the repo and fixed readme.md

--- code.py
+++ code.py
@@ -1,6 +1,13 @@
-def say_hello() -> None:
-    print("hello world!");
+# def say_hello(name String) -> None:
+#     print(name);
+
+
+# if __name__ == "__main__":
+#     say_hello(Sheldon);
+
+def say_hello(name):
+    print("hello " + name + "!");
 
 
 if __name__ == "__main__":
-    say_hello();
+    say_hello("Sheldon");
